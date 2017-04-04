a30c0a273b16524e7895a92a1c5d7a92e6645a0b
BDYLL <dyanez95@gmail.com>
2017-04-04 10:12:49 -0500

    Pushing start on timer creates a notification with the timer on it.

--- app/src/main/java/com/moviles/diego/proyectomoviles/TimerActivity.java
+++ app/src/main/java/com/moviles/diego/proyectomoviles/TimerActivity.java
@@ -1,10 +1,13 @@
 package com.moviles.diego.proyectomoviles;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +26,12 @@ public class TimerActivity extends AppCompatActivity {
 
     private Timer t;
 
+    private NotificationCompat.Builder builder;
+
+    private NotificationManager manager;
+
+    private static final int ID_NOTIFICATION=0;
+
     private final Handler h = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
@@ -34,7 +43,13 @@ public class TimerActivity extends AppCompatActivity {
             seconds     = seconds % 60;
             minutes = minutes%60;
 
-            timer.setText(String.format("%02d : %02d : %02d",hours,minutes,seconds));
+            String text = String.format("%02d : %02d : %02d",hours,minutes,seconds);
+
+            builder.setContentText(text);
+
+            manager.notify(ID_NOTIFICATION,builder.build());
+
+            timer.setText(text);
 
             return false;
         }
@@ -66,6 +81,16 @@ public class TimerActivity extends AppCompatActivity {
 
         this.name.setText(i.getStringExtra("activity"));
 
+        this.builder=new NotificationCompat.Builder(this);
+
+        this.builder
+                .setSmallIcon(R.drawable.timer)
+                .setContentTitle("Timer")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setOngoing(true);
+
+
+        this.manager=(NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
 
         this.start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +100,7 @@ public class TimerActivity extends AppCompatActivity {
                     t = new Timer();
                     t.schedule(new UpdateTimer(), 0, 1000);
                     isRunning = true;
+                    notification();
                 }
             }
         });
@@ -86,8 +112,16 @@ public class TimerActivity extends AppCompatActivity {
                     t.cancel();
                     t.purge();
                     isRunning=false;
+                    stopNotification();
                 }
             }
         });
+    }
+
+    private void notification(){
+        this.manager.notify(ID_NOTIFICATION,this.builder.build());
+    }
+    private void stopNotification(){
+        this.manager.cancel(ID_NOTIFICATION);
     }
 }
